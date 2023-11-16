1d28c1fa8f5080b56435b0b2cd811a176de0c6bb
orlovaaa <stt.orlova@gmail.com>
2023-11-16 16:47:08 +0300

    init

---
+++ resources/views/userProfile.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Profile</title>
+</head>
+<body>
+    <h1>User Profile</h1>
+    <p>Name: {{ $user->name }}</p>
+    <p>Email: {{ $user->email }}</p>
+    <p>ID: {{ $user->id }}</p>
+</body>
+</html>
